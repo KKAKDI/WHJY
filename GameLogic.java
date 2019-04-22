cc14fcb222781450cad700ce9558d4c93287b890
KKak_D <bullingo11@gmail.com>
2019-04-22 10:19:17 +0900

    게임로직 프로토콜 추가 #card1 #card2 #power #judge

--- GameLogic.java
+++ GameLogic.java
@@ -2,7 +2,7 @@
 import java.io.*; 
 import java.util.*;
 
-class GameLogic extends Thread{
+class GameLogic {
 
 	String[] strs = new String[] { "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15","16", "17", "18", "19", "20" };
 	Random r = new Random();
@@ -19,11 +19,7 @@ class GameLogic extends Thread{
 		this.sts = sts;
 		scm = new StuCM(sts);	
 		userCnt =size;
-	}
-	
-	public void run() {
-		cntUser();
-	}
+	}	
 
 	void cntUser() {
 		try {			
@@ -61,12 +57,12 @@ class GameLogic extends Thread{
 	      for (int i=0; i < userCnt; i++) { // À¯Àú ¼ö ÆÐ 2Àå ¼øÂ÷ µ¹¸²
 	          card1[i] = Integer.parseInt(hm.get(i)); // Ã¹ÆÐ	         
 	          cardMsg = hm.get(i);
-	          scm.broadcast("À¯Àú"+(i+1)+"ÀÇ Ã¹ ÆÐ"+cardMsg);
+	          scm.broadcast("#card1_"+cardMsg);
 	      }
 	      for(int j=0;j<userCnt;j++) {
 	          card2[j] = Integer.parseInt(hm.get(userCnt + j)); // µÎ¹øÂ° ÆÐ
 	          cardMsg = hm.get(userCnt + j);
-	          scm.broadcast("À¯Àú"+(j+1)+"ÀÇ µÎ¹øÂ° ÆÐ"+cardMsg);
+	          scm.broadcast("#card2_"+cardMsg);
 	      }
 	      for(int k=0;k<userCnt;k++) {
 	         p(card1[k] + " | " + card2[k] + ">>");
@@ -86,7 +82,7 @@ class GameLogic extends Thread{
 		for (int i = 0; i < userCnt; i++) { // ½Â¸® ÆÇº°
 			if (powers[i] == win) {
 				pln("user" + (i + 1) + " ½Â¸®");
-				scm.broadcast("À¯Àú"+(i+1)+"½Â¸®");
+				scm.broadcast("#judge_ÀÚ¸® "+i+" ½Â¸®");
 				draw++;
 				msg = i;
 			}
@@ -114,158 +110,158 @@ class GameLogic extends Thread{
 		// ±¤¶¯
 		if (card1 == 3 && card2 == 8) {
 			pln("ß² ø¢ ±¤ ¶¯");
-			scm.broadcast("ß² ø¢ ±¤ ¶¯");
+			scm.broadcast("#power_ß² ø¢ ±¤ ¶¯");
 			power = 100;
 		} else if (card1 == 1 && card2 == 8) {
 			pln("ìé ø¢ ±¤ ¶¯");
-			scm.broadcast("ìé ø¢ ±¤ ¶¯");
+			scm.broadcast("#power_ìé ø¢ ±¤ ¶¯");
 			power = 90;
 		} else if (card1 == 1 && card2 == 3) {
 			pln("ìé ß² ±¤ ¶¯");
-			scm.broadcast("ìé ß² ±¤ ¶¯");
+			scm.broadcast("#power_ìé ß² ±¤ ¶¯");
 			power = 80;
 		}
 		// ¶¯
 		else if (cardSub == 10 || cardSub == -10) {
 			if (card1 == 10 || card1 == 20) {
 				pln("Àå ¶¯");
-				scm.broadcast("Àå ¶¯");
+				scm.broadcast("#power_Àå ¶¯");
 				power = 27;
 			}
 			if (card1 == 9 || card1 == 19) {
 				pln("±¸ ¶¯");
-				scm.broadcast("±¸ ¶¯");
+				scm.broadcast("#power_±¸ ¶¯");
 				power = 25;
 			}
 			if (card1 == 8 || card1 == 18) {
 				pln("ÆÈ ¶¯");
-				scm.broadcast("ÆÈ ¶¯");
+				scm.broadcast("#power_ÆÈ ¶¯");
 				power = 24;
 			}
 			if (card1 == 7 || card1 == 17) {
 				pln("Ä¥ ¶¯");
-				scm.broadcast("Ä¥ ¶¯");
+				scm.broadcast("#power_Ä¥ ¶¯");
 				power = 23;
 			}
 			if (card1 == 6 || card1 == 16) {
 				pln("À° ¶¯");
-				scm.broadcast("À° ¶¯");
+				scm.broadcast("#power_À° ¶¯");
 				power = 22;
 			}
 			if (card1 == 5 || card1 == 15) {
 				pln("¿À ¶¯");
-				scm.broadcast("¿À ¶¯");
+				scm.broadcast("#power_¿À ¶¯");
 				power = 21;
 			}
 			if (card1 == 4 || card1 == 14) {
 				pln("»ç ¶¯");
-				scm.broadcast("»ç ¶¯");
+				scm.broadcast("#power_»ç ¶¯");
 				power = 20;
 			}
 			if (card1 == 3 || card1 == 13) {
 				pln("»ï ¶¯");
-				scm.broadcast("»ï ¶¯");
+				scm.broadcast("#power_»ï ¶¯");
 				power = 19;
 			}
 			if (card1 == 2 || card1 == 12) {
 				pln("ÀÌ ¶¯");
-				scm.broadcast("ÀÌ ¶¯");
+				scm.broadcast("#power_ÀÌ ¶¯");
 				power = 18;
 			}
 			if (card1 == 1 || card1 == 11) {
 				pln("ÀÏ ¶¯");
-				scm.broadcast("»æ ¶¯");
+				scm.broadcast("#power_»æ ¶¯");
 				power = 17;
 			}
 		}
 		// Áß°£ Á·º¸
 		else if ((card1 == 1 || card1 == 11) && (card2 == 2 || card2 == 12)) {
 			pln("¾Ë ¸®");
-			scm.broadcast("¾Ë ¸®");
+			scm.broadcast("#power_¾Ë ¸®");
 			power = 15;
 		} else if ((card1 == 1 || card1 == 11) && (card2 == 4 || card2 == 14)) {
 			pln("µ¶ »ç");
-			scm.broadcast("µ¶ »ç");
+			scm.broadcast("#power_µ¶ »ç");
 			power = 14;
 		} else if ((card1 == 1 || card1 == 11) && (card2 == 9 || card2 == 19)) {
 			pln("±¸ »æ");
-			scm.broadcast("±¸ »æ");
+			scm.broadcast("#power_±¸ »æ");
 			power = 13;
 		} else if ((card1 == 1 || card1 == 11) && (card2 == 10 || card2 == 20)) {
 			pln("Àå »æ");
-			scm.broadcast("Àå »æ");
+			scm.broadcast("#power_Àå »æ");
 			power = 12;
 		} else if ((card1 == 4 || card1 == 14) && (card2 == 10 || card2 == 20)) {
 			pln("Àå »ç");
-			scm.broadcast("Àå »ç");
+			scm.broadcast("#power_Àå »ç");
 			power = 11;
 		} else if ((card1 == 4 || card1 == 14) && (card2 == 6 || card2 == 16)) {
 			pln("¼¼ ·ú");
-			scm.broadcast("¼¼ ·ú");
+			scm.broadcast("#power_¼¼ ·ú");
 			power = 10;
 		}
 		// Æ¯¼ö Á·º¸
 		else if (card1 == 4 && card2 == 7) {
 			pln("¾Ï Çà ¾î »ç");
-			scm.broadcast("¾Ï Çà ¾î »ç");
+			scm.broadcast("#power_¾Ï Çà ¾î »ç");
 			if(win>80)power=99;
 			else power=1;
 		}
 		else if (card1 == 3 && card2 == 7) {
 			pln("¶¯ Àâ ÀÌ");			
-			scm.broadcast("¶¯ Àâ ÀÌ");
+			scm.broadcast("#power_¶¯ Àâ ÀÌ");
 			if(win<26&&win>16)power=40;
 			else power =0;
 		}
 		else if (card1 == 4 && card2 == 9) {
 			pln("¸Û ÅÖ ±¸ ¸® »ç ±¸");
-			scm.broadcast("¸Û ÅÖ ±¸ ¸® »ç ±¸");
+			scm.broadcast("#power_¸Û ÅÖ ±¸ ¸® »ç ±¸");
 			power = 26;
 		} else if ((card1 == 4 && card2 == 19) || (card1 == 14 && card2 == 9)) {
 			pln("»ç ±¸ ÆÄ Åä");
-			scm.broadcast("»ç ±¸ ÆÄ Åä");
+			scm.broadcast("#power_»ç ±¸ ÆÄ Åä");
 			power = 16;
 		}
 		// ²ý
 		else if (rest == 9) {
 			pln("°© ¿À");
-			scm.broadcast("°© ¿À");
+			scm.broadcast("#power_°© ¿À");
 			power = 9;
 		} else if (rest == 8) {
 			pln("¿©´ü ²ý");
-			scm.broadcast("¿©´ü ²ý");
+			scm.broadcast("#power_¿©´ü ²ý");
 			power = 8;
 		} else if (rest == 7) {
 			pln("ÀÏ°ö ²ý");
-			scm.broadcast("ÀÏ°ö ²ý");
+			scm.broadcast("#power_ÀÏ°ö ²ý");
 			power = 7;
 		} else if (rest == 6) {
 			pln("¿©¼¸ ²ý");
-			scm.broadcast("¿©¼¸ ²ý");
+			scm.broadcast("#power_¿©¼¸ ²ý");
 			power = 6;
 		} else if (rest == 5) {
 			pln("´Ù¼¸ ²ý");
-			scm.broadcast("´Ù¼¸ ²ý");
+			scm.broadcast("#power_´Ù¼¸ ²ý");
 			power = 5;
 		} else if (rest == 4) {
 			pln("³× ²ý");
-			scm.broadcast("³× ²ý");
+			scm.broadcast("#power_³× ²ý");
 			power = 4;
 		} else if (rest == 3) {
 			pln("¼¼ ²ý");
-			scm.broadcast("¼¼ ²ý");
+			scm.broadcast("#power_¼¼ ²ý");
 			power = 3;
 		} else if (rest == 2) {
 			pln("µÎ ²ý");
-			scm.broadcast("µÎ ²ý");
+			scm.broadcast("#power_µÎ ²ý");
 			power = 2;
 		} else if (rest == 1) {
 			pln("ÇÑ ²ý");
-			scm.broadcast("ÇÑ ²ý");
+			scm.broadcast("#power_ÇÑ ²ý");
 			power = 1;
 		} else if (rest == 0) {
 			pln("¸Á Åë");
-			scm.broadcast("¸Á Åë");
+			scm.broadcast("#power_¸Á Åë");
 			power = 0;
 		}
 		return power;
